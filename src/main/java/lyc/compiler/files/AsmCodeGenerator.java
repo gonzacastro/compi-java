26a475923fc35a2ec120c62735028a8a3d2acbd0
Tomas Serravento <81922863+tomasserravento@users.noreply.github.com>
2022-11-26 18:41:09 -0300

    Prueba

--- src/main/java/lyc/compiler/files/AsmCodeGenerator.java
+++ src/main/java/lyc/compiler/files/AsmCodeGenerator.java
@@ -9,4 +9,8 @@ public class AsmCodeGenerator implements FileGenerator {
     public void generate(FileWriter fileWriter) throws IOException {
         fileWriter.write("TODO");
     }
+
+    public String obtenerCabecera(){
+        return ".CODE\nSTART:\nmov AX,@DATA\nmov DS,AX\nmov es,ax\n\n";
+    }
 }
